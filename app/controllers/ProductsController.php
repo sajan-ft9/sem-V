76d8bbe26e60158157f8312b606375d1464c2d9d
sajan-ft9 <sajankhad2@gmail.com>
2021-06-25 13:23:30 +0545

    mvc

---
+++ app/controllers/ProductsController.php
@@ -0,0 +1,9 @@
+<?php 
+    require_once "..\app\models\dbh.php";
+    class ProductsController extends Dbh {
+
+        public function viewProducts() {
+            
+        }
+    }
+
---
+++ app/init.php
@@ -0,0 +1,7 @@
+<?php 
+
+require_once "../app\controllers\ProductsController.php";
+require_once "../app/views\productsview.php";
+
+$Products = new ProductsView();
+$ProductsContr = new ProductsController();
